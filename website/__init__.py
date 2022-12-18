436e984348d1f00f3baa76df6cc80e2cf510f3e6
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-12-18 20:46:52 +0530

    Fix Circular Import Error

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,3 @@
-from website import routes
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,3 +5,5 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///website.db'
 app.config['SECRET_KEY'] = 'bf20701a8f6a885c1206ddff'
+
+from website import routes # Important to keep this at the last
