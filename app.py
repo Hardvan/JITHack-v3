6ff14b1733df0f935f2daa594fd2906ff021f847
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-12-18 19:39:33 +0530

    Tweaks in routes.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from website import app
 
 if __name__ == '__main__':
- app.run(debug=True)
+    app.run(debug=True)
+
+# Setup:
+# ? set FLASK_APP=app.py
+# ? Run the app.py file
--- website/routes.py
+++ website/routes.py
@@ -1,5 +1,5 @@
 from website import app
-from flask import render_template,redirect, url_for,request
+from flask import render_template, redirect, url_for, request
 from website.forms import DataForm
 import tensorflow as tf
 import pandas as pd
@@ -7,28 +7,39 @@ import pandas as pd
 age = -1
 chol = -1
 cp = -1
+
+
 @app.route('/')
-@app.route('/home', methods = ["GET","POST"])
+@app.route('/home', methods=["GET", "POST"])
 def home_page():
-    model = tf.keras.models.load_model("my_model.h5")
     global age
     global chol
     global cp
     result = -1
+
+    model = tf.keras.models.load_model(
+        "Model/saved_model/my_model.h5")
+
     if request.method == "POST":
         data_form = dict(request.form)
-        age = int(request.form.get('age'))
-        chol = int(request.form.get('chol'))
-        cp = int(request.form.get('cp'))
+
+        # For Personalised Dietary Plan
+        age, chol, cp = int(data_form["age"]), int(
+            data_form["chol"]), int(data_form["cp"])
+
         for i in data_form.copy():
             data_form[i] = float(data_form[i])
+
+        # Converting the data into a dataframe
         df = pd.DataFrame([data_form])
-        result = model.predict(df)
+
+        # Predicting the result
+        result = model.predict(df)  # [[0.]] or [[1.]]
         result = int(result[0][0])
-        print (result)
+        print(result)
 
+    return render_template('index.html', result=result)
 
-    return render_template('index.html',result = result)
 
 @app.route('/blog')
 def blog_page():
@@ -39,7 +50,8 @@ def blog_page():
     chol1 = chol
     cp1 = cp
 
-    return render_template('here.html',age = age1, chol = chol1, cp = cp1)
+    return render_template('here.html', age=age1, chol=chol1, cp=cp1)
+
 
 @app.route('/about')
 def about_page():
