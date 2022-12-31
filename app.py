ceb5b6c4ce38829c264b13c1f18b6e59cdebb118
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-12-31 17:42:23 +0530

    Simplify the code

--- app.py
+++
@@ -1,8 +0,0 @@
-from website import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-# Setup:
-# ? set FLASK_APP=app.py
-# ? Run the app.py file
--- website/app.py
+++ website/app.py
@@ -1,8 +1,10 @@
-from website import app
-from flask import render_template, redirect, url_for, request
-from website.forms import DataForm
-import tensorflow as tf
+from flask import Flask, render_template, request
 import pandas as pd
+import tensorflow as tf
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+
 
 age = -1
 chol = -1
@@ -18,7 +20,7 @@ def home_page():
     result = -1
 
     model = tf.keras.models.load_model(
-        "Model/saved_model/my_model.h5")
+        "../Model/saved_model/my_model.h5")
 
     if request.method == "POST":
         data_form = dict(request.form)
@@ -38,7 +40,7 @@ def home_page():
         result = int(result[0][0])
         print(result)
 
-    return render_template('index.html', result=result)
+    return render_template("index.html", result=result)
 
 
 @app.route('/blog')
@@ -56,3 +58,11 @@ def blog_page():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Setup:
+# ? set FLASK_APP=app.py
+# ? Run the app.py file
